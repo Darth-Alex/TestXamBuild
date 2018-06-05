66bf2d1d738e27e9a068d14ded6834a31280bdaa
Alexander Rummel <alexander.rummel@it-factum.de>
2018-06-05 17:23:04 +0200

    no message

--- TestApp/TestApp.Android/obj/Debug/android/src/mono/MonoPackageManager.java
+++ TestApp/TestApp.Android/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -108,6 +108,7 @@ class MonoPackageManager_Resources {
 		/* We need to ensure that "TestApp.Android.dll" comes first in this list. */
 		"TestApp.Android.dll",
 		"FormsViewGroup.dll",
+		"Newtonsoft.Json.dll",
 		"TestApp.dll",
 		"Xamarin.Android.Arch.Core.Common.dll",
 		"Xamarin.Android.Arch.Lifecycle.Common.dll",
--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace TestApp
 {
     public partial class MainPage : ContentPage
     {
+        class tmp{
+            public string test => "ABC";
+        }
+     
         public MainPage()
         {
             InitializeComponent();
+
+            tmp temp = new tmp();
+            string res = JsonConvert.SerializeObject(temp);
         }
     }
 }
